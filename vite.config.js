4a1768f165bcba2572ceeb247bf131e7e3df5166
omarkaraksi <omar.karaksi09@gmail.com>
2025-01-31 19:07:32 +0200

    testing changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/omarkaraksi/freshcart-ecommerce', // Replace with your repo name
+  base: 'https://omarkaraksi.github.io/freshcart-ecommerce', // Replace with your repo name
 
 })
