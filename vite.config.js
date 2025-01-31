887580ff891a80dde0e63c53b31bb92619d07de9
omarkaraksi <omar.karaksi09@gmail.com>
2025-01-31 18:46:28 +0200

    vite config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/repo-name/', // Replace with your repo name
+  base: '/freshcart-ecommerce/', // Replace with your repo name
 
 })
